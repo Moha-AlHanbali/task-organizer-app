3ebff742d7fde3418494b8f9eadc0aaac0826822
Moha-AlHanbali <moha.alhanbali@gmail.com>
2022-01-30 14:40:44 +0200

    feat: login component basic style

--- components/Login.js
+++ components/Login.js
@@ -7,32 +7,43 @@ export default function Login({ login }) {
     return (
 
         <>
-            <div>
-                <form onSubmit={(event) => {event.preventDefault(), login(event.target.userName.value, event.target.password.value)}}>
-                    <div>
-                        <label id='userName'>
-                            username
-                        </label>
-                        <input name='userName' id='userName' placeholder='USERNAME' required />
-                    </div>
+            <div className='flex h-screen '>
+                <div className='px-10 py-10 m-auto border-4 border-orange-600 rounded-lg md:w-1/3'>
+                    <form onSubmit={(event) => { event.preventDefault(), login(event.target.userName.value, event.target.password.value) }}>
+                        <div className='mb-6 md:flex md:items-center'>
+                            <div className='md:w-1/4'>
+                                <label htmlFor='userName' className='font-bold text-gray-700 text-md'>
+                                    Username
+                                </label>
+                            </div>
+                            <div className='w-3/4'>
+                                <input name='userName' id='userName' placeholder='username' type='text' required className='w-full px-3 py-2 leading-tight text-gray-700 border rounded appearance-none focus:outline-none ' />
+                            </div>
+                        </div>
 
-                    <div>
-                        <label id='password'>
-                            password
-                        </label>
-                        <input name='password' id='password' placeholder='PASSWORD' type='password' required />
-                    </div>
+                        <div className='mb-6 md:flex md:items-center'>
+                            <div className='md:w-1/4'>
+                                <label htmlFor='password' className='font-bold text-gray-700 text-md'>
+                                    Password
+                                </label>
+                            </div>
+                            <div className='w-3/4'>
+                                <input name='password' id='password' placeholder='password' type='password' required className='w-full px-3 py-2 leading-tight text-gray-700 border rounded appearance-none focus:outline-none ' />
+                            </div>
+                        </div>
 
-                    <div>
-                        <button type='submit'>
-                            login
-                        </button>
-                    </div>
-                </form>
-            </div>
+                        <div className='flex items-center justify-between mt-10'>
+                            <button type='submit' className='w-2/6 px-4 py-2 font-bold text-white bg-blue-500 rounded shadow hover:bg-orange-500 focus:shadow-outline focus:outline-none" type="button'>
+                                Login
+                            </button>
 
-            <div>
-                <p>Not a member yet? <a onClick={() => {router.push('/register')}}> Sign Up here!</a></p>
+                            <div className='w-2/6'>
+                            </div>
+                            <button onClick={() => { router.push('/register') }} type='submit' className='w-2/6 px-4 py-2 font-bold text-white bg-orange-500 rounded shadow hover:bg-blue-500 focus:shadow-outline focus:outline-none" type="button'> Sign Up </button>
+                        </div>
+                    </form>
+                </div>
+
             </div>
         </>
 
--- components/Signup.js
+++ components/Signup.js
@@ -29,63 +29,66 @@ export default function Signup() {
       'age': String(e.target.age.value)
     }
 
-try{
-  const response = await axios.post(register_url, accountInfo)
-  if (response.status == 201) {
-    alert('Account successfully created')
-    login(e.target.userName.value, e.target.password1.value)
-    router.push('/')
-  }
-}
-catch(err){
-  console.log(err);
-}
+    try {
+      const response = await axios.post(register_url, accountInfo)
+      if (response.status == 201) {
+        alert('Account successfully created')
+        login(e.target.userName.value, e.target.password1.value)
+        router.push('/')
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
 
   return (
-    <div>
-      <a onClick={() => {router.push('/')}}>Return</a>
-      <form onSubmit={handleRegister}>
+    <div className='flex h-screen '>
+      <div className='px-10 py-10 m-auto border-4 border-blue-600 rounded-lg md:w-1/3'>
+        <a onClick={() => { router.push('/') }}>back</a>
+        <form onSubmit={handleRegister}>
 
-        <div>
-          <label id='userName'> username </label>
-          <input name='userName' id='userName' placeholder='USERNAME' required />
-        </div>
+          <div>
+            <label id='userName'> Username </label>
+            <input name='userName' id='userName' placeholder='username' required />
+          </div>
 
-        <div>
-          <label id='email'> email </label>
-          <input name='email' id='email' placeholder='EMAIL' type='email' required />
-        </div>
+          <div>
+            <label id='email'> E-mail </label>
+            <input name='email' id='email' placeholder='example@host.com' type='email' required />
+          </div>
 
-        <div>
-          <label id='firstName'> first name </label>
-          <input name='firstName' id='firstName' placeholder='FIRST NAME' required />
-        </div>
+          <div>
+            <label id='firstName'> First Name </label>
+            <input name='firstName' id='firstName' placeholder='John' required />
+          </div>
 
-        <div>
-          <label id='lastName'> first name </label>
-          <input name='lastName' id='lastName' placeholder='LAST NAME' required />
-        </div>
+          <div>
+            <label id='lastName'> first name </label>
+            <input name='lastName' id='lastName' placeholder='Smith' required />
+          </div>
 
-        <div>
-          <label id='password1'> password </label>
-          <input name='password1' id='password1' placeholder='PASSWORD' type='password' pattern="(?=.~*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
-        </div>
+          <div>
+            <label id='password1'> Password </label>
+            <input name='password1' id='password1' placeholder='password' type='password' pattern="(?=.~*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+          </div>
 
-        <div>
-          <label id='password2'>re-enter password</label>
-          <input name='password2' id='password2' placeholder='PASSWORD' type='password' pattern="(?=.~*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
-        </div>
+          <div>
+            <label id='password2'>Confirm Password</label>
+            <input name='password2' id='password2' placeholder='re-enter password' type='password' pattern="(?=.~*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+          </div>
 
-        <div>
-          <label id='age'> age </label>
-          <input name='age' id='age' type='date' required />
-        </div>
+          <div>
+            <label id='age'> Birth date </label>
+            <input name='age' id='age' type='date' required />
+          </div>
 
-        <div>
-          <button type='submit'> submit </button>
-        </div>
-      </form>
+          <div>
+            <button type='submit'> Submit </button>
+          </div>
+        </form>
+      </div>
     </div>
+
   );
 }
